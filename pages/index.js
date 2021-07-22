686da53a649e8f8950fde262f88c052b9231bb0e
Omar Silva <omarsilva@outlook.com>
2021-07-22 13:40:33 -0500

    Demo finished

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,53 @@
+// For handling input states
+import { useState } from "react";
+
+// For display toasts
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  // Input states
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [purpose, setPurpose] = useState("");
+  const [message, setMessage] = useState("");
+
+  // Form submit handler
+  const submitForm = async (e) => {
+    e.preventDefault();
+    const res = await fetch("http://localhost:3000/api/submit-form", {
+      method: "POST",
+      body: JSON.stringify({ name, email, purpose, message }),
+    });
+    console.log(process.env.NEXT_PUBLIC_NOTION_API_KEY);
+    // Success if status code is 201
+    if (res.status === 201) {
+      toast("Thank you for contacting us!", { type: "success" });
+    } else {
+      toast("Please re-check your inputs.", { type: "error" });
+    }
+  };
+
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
+      <ToastContainer />
+      <form className={styles.form} onSubmit={submitForm}>
         <h1 className={styles.title}>React Out To Us</h1>
         <div>
           <label htmlFor="name">Full Name</label>
-          <input type="text" id="name" name="name" placeholder="John Doe" required />
+          <input type="text" id="name" name="name" placeholder="John Doe" value={name} onChange={(e) => setName(e.target.value)} required />
         </div>
         <div className={styles.inputs}>
           <div>
             <label htmlFor="email">E-Mail Address</label>
-            <input type="email" name="email" placeholder="johndoe@example.io" required />
+            <input type="email" name="email" placeholder="johndoe@example.io" value={email} onChange={(e) => setEmail(e.target.value)} required />
           </div>
           <div>
             <label htmlFor="purpose">Purpose</label>
-            <select name="purpose" id="purpose">
-              <option value="" selected disabled required>
+            <select name="purpose" id="purpose" value={purpose} onChange={(e) => setPurpose(e.target.value)}>
+              <option value="" disabled required>
                 Select one
               </option>
               <option value="Web Development">Web Development</option>
@@ -29,7 +59,7 @@ export default function Home() {
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" rows="5" placeholder="Hi there!" required></textarea>
+          <textarea name="message" id="message" rows="5" placeholder="Hi there!" value={message} onChange={(e) => setMessage(e.target.value)} required></textarea>
         </div>
         <button className={styles.btn} type="submit">
           Submit
